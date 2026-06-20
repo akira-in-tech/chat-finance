f2db0b2f60143cfead145c8743005e7d8a35f2d5
akira <ye.ton@northeastern.edu>
2026-06-20 03:37:22 -0700

    security: remove hardcoded OpenAI API key from config.py, use env var

--- ChatFinance/APIs/config.py
+++ ChatFinance/APIs/config.py
@@ -1,10 +1,9 @@
-OPENAI_API_KEY = "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"
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Path to persist the index
-INDEX_PERSIST_DIR = os.path.join(BASE_DIR, "../index_persist")
-
-# Path to the directory containing HTML news files
-NEWS_DATA = os.path.join(BASE_DIR, "../news_crawler/news_data")
+INDEX_PERSIST_DIR = os.getenv("INDEX_PERSIST_DIR", os.path.join(BASE_DIR, "../index_persist"))
+NEWS_DATA = os.getenv("NEWS_DATA_DIR", os.path.join(BASE_DIR, "../news_crawler/news_data"))
